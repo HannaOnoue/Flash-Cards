2e2af5858ca05ce6ab0add224c849643f5af6bdc
HannaOnoue <hannabaratinha@gmail.com>
2024-11-25 11:56:12 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Filme',
+    'Quem é o nome do camaleão da Rapunzel?',
+    'Pascal'
+)
+
+criaCartao(
+    'Série',
+    'Qual a personagem de Outer Banks que foi forçada a ir para um internato para meninas?',
+    'Kiara Carrera'
+)
+
+criaCartao(
+    'Filme',
+    'Qual o nome verdadeiro do Flin Rider?',
+    'José Bezerra'
+)
+
+criaCartao(
+    'Filme',
+    'Qual o nome do gato da madrasta da Cinderela?',
+    'Lúcifer'
+)
+
+criaCartao(
+    'Série',
+    'Quem é o personagem de Stranger Things que possui poderes?',
+    'Eleven (11)'
+)
